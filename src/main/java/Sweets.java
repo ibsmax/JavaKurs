aab178519278d45f1a0ac8fd1a1a0d1ddd7def01
Max <ibsmax23@gmail.com>
2021-05-08 20:57:40 +0500

    Added Task4_part2

---
+++ src/main/java/Sweets.java
@@ -0,0 +1,31 @@
+public class Sweets {
+    public static int getPriceCandy() {
+        int price1 = 20;
+        return price1;
+    }
+
+    public static int getPriceJellybean() {
+        int price2 = 15;
+        return price2;
+    }
+
+    public static int getPriceKitkat() {
+        int price3 = 30;
+        return price3;
+    }
+
+    public static int getWeightCandy() {
+        int weight1 = 50;
+        return weight1;
+    }
+
+    public static int getWeightJellybean() {
+        int weight2 = 45;
+        return weight2;
+    }
+
+    public static int getWeightKitkat() {
+        int weight3 = 65;
+        return weight3;
+    }
+}
