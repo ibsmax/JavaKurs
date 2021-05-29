930f7c73c61da389e4b30ad562f8d9c4e7e07a1e
Max <ibsmax23@gmail.com>
2021-05-29 08:47:48 +0500

    Fixed Task8

--- src/test/java/OperationsTests.java
+++ src/test/java/OperationsTests.java
@@ -11,22 +11,35 @@ public class OperationsTests {
         per2 = 5.0;
         Assert.assertEquals(15.0, GetResultAdditional(per1, per2), 0);
     }
+
     @Test
     public void SubtractionResultTest() {
         per1 = 10.0;
         per2 = 5.0;
         Assert.assertEquals(5.0, GetResultSubstraction(per1, per2), 0);
     }
+
     @Test
     public void MultiplicationResultTest() {
         per1 = 10.0;
         per2 = 5.0;
         Assert.assertEquals(50.0, GetResultMultiplication(per1, per2), 0);
     }
+
     @Test
     public void DivisionResultTest() {
         per1 = 10.0;
         per2 = 5.0;
         Assert.assertEquals(2.0, GetResultDivision(per1, per2), 0);
+    }
+
+    @Test
+    public void DivisionZeroTest() {
+        per1 = 10.0;
+        per2 = 0.0;
+        if (per2 == 0.0) {
+            System.out.println("Попытка деления на ноль!");
+        }
+        Assert.assertEquals(true, GetResultDivision(per1, per2));
     }
 }
