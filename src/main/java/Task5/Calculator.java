b40f54238996e38f306fbf846d6f686cb7f64b68
Max <ibsmax23@gmail.com>
2021-05-29 10:45:27 +0500

    Fixed Task5 + Task7

--- src/main/java/Task5/Calculator.java
+++ src/main/java/Task5/Calculator.java
@@ -1,17 +1,11 @@
 package Task5;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public abstract class Calculator {
     public static Double per1;
     public static Double per2;
-
-    public static Double getPer1() {
-        return per1;
-    }
-
-    public static Double getPer2() {
-
-        return per2;
-    }
 
     public Calculator(Double per1, Double per2) {
         this.per1 = per1;
@@ -19,19 +13,57 @@ public abstract class Calculator {
     }
 
     public static Double GetResultAdditional(Double per1, Double per2) {
-
-        return getPer1() + getPer2();
+        return per1 + per2;
     }
 
     public static Double GetResultMultiplication(Double per1, Double per2) {
-        return getPer1() * getPer2();
+        return per1 * per2;
     }
 
     public static Double GetResultSubstraction(Double per1, Double per2) {
-        return getPer1() - getPer2();
+        return per1 - per2;
     }
 
     public static Double GetResultDivision(Double per1, Double per2) {
-        return getPer1() / getPer2();
+        return per1 / per2;
+    }
+
+    public static Double InputNumber() {
+        System.out.println("Введите число");
+        Scanner scanner = new Scanner(System.in);
+        Double number = scanner.nextDouble();
+        return number;
+    }
+
+
+    public static char ChoiceOperation() {
+        System.out.println("Введите знак операции (+,-,/,*)");
+        Scanner scanner = new Scanner(System.in);
+        char oper = scanner.next().charAt(0);
+        return oper;
+    }
+
+    public static Double Computation(double per1, double per2, char oper) {
+
+
+        if (oper == '+') {
+            double resultComputation = GetResultAdditional(per1, per2);
+            return resultComputation;
+        }
+        if (oper == '-') {
+
+            double resultComputation = GetResultSubstraction(per1, per2);
+            return resultComputation;
+        }
+        if (oper == '/') {
+
+            double resultComputation = GetResultDivision(per1, per2);
+            return resultComputation;
+        }
+        if (oper == '*') {
+
+            double resultComputation = GetResultMultiplication(per1, per2);
+            return resultComputation;
+        } else return null;
     }
 }
--- src/main/java/Task5/Main.java
+++ src/main/java/Task5/Main.java
@@ -1,40 +1,39 @@
 package Task5;
 
+import javax.xml.stream.FactoryConfigurationError;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static Task5.Calculator.*;
+import static Task5.Calculator.per2;
 
 public class Main {
-    /** Задание 5 (Реализовать калькулятор в стиле ООП. Архитектуру приложения продумать самостоятельно.)
-     @author Pystogov Maxim
+    /**
+     * Задание 5 (Реализовать калькулятор в стиле ООП. Архитектуру приложения продумать самостоятельно.)
+     *
+     * @author Pystogov Maxim
      */
-    public static void main(String[] args) {
-        System.out.println("Введите первое число");
-        Scanner scanner = new Scanner(System.in);
-        Double per1 = scanner.nextDouble();
-        System.out.println("Введите знак операции (+,-,/,*)");
-        String oper = scanner.next();
-        System.out.println("Введите второе число");
-        Double per2 = scanner.nextDouble();
-        if (oper.equals("+")) {
-            Calculator.per1 = per1;
-            Calculator.per2 = per2;
-            System.out.println("Результат вычисления: "+per1+ " "+oper+" "+per2+" = "+ GetResultAdditional(Calculator.per1, Calculator.per2));
-        }
-        if (oper.equals("-")) {
-            Calculator.per1 = per1;
-            Calculator.per2 = per2;
-            System.out.println("Результат вычисления: "+per1+ " "+oper+" "+per2+" = " + GetResultSubstraction(Calculator.per1, Calculator.per2));
-        }
-        if (oper.equals("/")) {
-            Calculator.per1 = per1;
-            Calculator.per2 = per2;
-            System.out.println("Результат вычисления: "+per1+ " "+oper+" "+per2+" = " + GetResultDivision(Calculator.per1, Calculator.per2));
-        }
-        if (oper.equals("*")) {
-            Calculator.per1 = per1;
-            Calculator.per2 = per2;
-            System.out.println("Результат вычисления: "+per1+ " "+oper+" "+per2+" = " + GetResultMultiplication(Calculator.per1, Calculator.per2));
+    public static void main(String[] args) throws InputMismatchException, NullPointerException, ClassCastException {
+        try {
+            Double per1 = InputNumber();
+            char oper = ChoiceOperation();
+            Double per2 = InputNumber();
+            double res = Computation(per1, per2, oper);
+            if (res == Double.POSITIVE_INFINITY ||
+                    res == Double.NEGATIVE_INFINITY)
+                throw new ArithmeticException();
+            System.out.println("Результат вычисления: " + per1 + " " + oper + " " + per2 + " = " + res);
+        } catch (InputMismatchException e) {
+            System.out.print("При вводе нецелого числа используйте запятые вместо точек, в качестве разделителя!");
+        } catch (NullPointerException e) {
+            System.out.print("Ошибка при вводе переменных!");
+        } catch (ClassCastException e) {
+            System.out.print("Выполнена неверная операция преобразования типов!");
+        } catch (ArithmeticException e) {
+            System.out.println("Попытка деления на 0 !");
         }
     }
 }
+
+
+
